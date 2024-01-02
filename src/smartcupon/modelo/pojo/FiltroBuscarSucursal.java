a2f70d559d515c3f23d1bb6c7948fc9b542c18ce
Manuel Hernandez <mahmaruro@outlook.com>
2024-01-02 01:23:54 -0600

    implementar buscar sucursales por filtro

---
+++ src/smartcupon/modelo/pojo/FiltroBuscarSucursal.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package smartcupon.modelo.pojo;
+
+/**
+ *
+ * @author Manuel Hernandez
+ */
+public class FiltroBuscarSucursal {
+
+    private String nombre;
+    private String direccion;
+
+    public FiltroBuscarSucursal() {
+    }
+
+    public FiltroBuscarSucursal(String nombre, String direccion) {
+        this.nombre = nombre;
+        this.direccion = direccion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+}

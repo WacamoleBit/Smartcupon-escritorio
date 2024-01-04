93fbb14bbc5c90f99e8822409a17e6f1d0892298
Manuel Hernandez <mahmaruro@outlook.com>
2024-01-04 06:03:51 -0600

    generar pantalla promocion sucursal

---
+++ src/smartcupon/escritorio/FXMLPromocionSucursalController.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package smartcupon.escritorio;
+
+import java.net.URL;
+import java.util.List;
+import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import smartcupon.modelo.dao.SucursalDAO;
+import smartcupon.modelo.pojo.FiltroBuscarPromocion;
+import smartcupon.modelo.pojo.FiltroBuscarSucursal;
+import smartcupon.modelo.pojo.Mensaje;
+import smartcupon.modelo.pojo.Sucursal;
+import smartcupon.utils.Utilidades;
+
+/**
+ * FXML Controller class
+ *
+ * @author jegal
+ */
+public class FXMLPromocionSucursalController implements Initializable {
+
+    @FXML
+    private TableView<Sucursal> tvSucursales;
+    @FXML
+    private TableColumn<?, ?> colNombre;
+    @FXML
+    private TableColumn<?, ?> colTelefono;
+    @FXML
+    private TableColumn<?, ?> colLatitud;
+    @FXML
+    private TableColumn<?, ?> colLongitud;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+    @FXML
+    private void btnEliminar(ActionEvent event) {
+    }
+
+}
